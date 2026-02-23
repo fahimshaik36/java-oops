251bf66e470fdae6eb33288577a76453fa6601c6
fahimshaik36 <fahimshaik656@gmail.com>
2026-02-23 11:22:04 +0530

    Updated OOPS

--- src/com/fahim/oops/AbstractDemo.java
+++ src/com/fahim/oops/AbstractDemo.java
@@ -1,5 +1,76 @@
 package com.fahim.oops;
 
+abstract class Bird{
+	abstract void eats();
+	abstract void fly();
+}
+
+abstract class Eagle extends Bird{
+	abstract void eats();
+	void fly() {
+		System.out.println("Eagle flies at greater heights");
+	}
+}
+
+class SerpentEagle extends Eagle{
+	@Override
+	void eats() {
+		System.out.println("Serpent eagle eats Snakes and Reptiles");
+	}
+	
+}
+
+class GoldenEagle extends Eagle{
+	void eats() {
+		System.out.println("Golden eagle eats mammals or fishes");
+	}
+}
+
+class Sky{
+	public void EagleFamily(Eagle e){
+		e.fly();
+		e.eats();
+	}
+}
+
 public class AbstractDemo {
+	public static void main(String[] args) {
+		
+		SerpentEagle se = new SerpentEagle();
+		GoldenEagle ge = new GoldenEagle();
+		Sky s = new Sky();
+		
+		s.EagleFamily(se);
+		
+		System.out.println();
+		
+		s.EagleFamily(ge);
+		
+		
+		
+//		Eagle e;
+//		e = new SerpentEagle();
+//		e.eats();
+//		e.fly();
+//		
+//		System.out.println();
+//		
+//		e= new GoldenEagle();
+//		e.eats();
+//		e.fly();
+	
+		
+//		SerpentEagle se = new SerpentEagle();
+//		GoldenEagle ge = new GoldenEagle();
+//		
+//		se.fly();
+//		se.eats();
+//		
+//		System.out.println();
+//		
+//		ge.fly();
+//		ge.eats();
+	}
 
 }
+
--- src/com/fahim/oops/AggregationAndCompositionDemo.java
+++ src/com/fahim/oops/AggregationAndCompositionDemo.java
@@ -1,5 +1,159 @@
 package com.fahim.oops;
 
-public class AggregationAndCompositionDemo {
+class Heart {
+	private int weight;
+	private int BPM;
+	
+	public Heart(int weight, int BPM) {
+		this.weight = weight;
+		this.BPM = BPM;
+	}
+	
+	public int getWeight() {
+		return weight;
+	}
 
+	public int getBPM() {
+		return BPM;
+	}
+
+	public void setBPM(int BPM) {
+		this.BPM = BPM;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
 }
+
+class Brain{
+	private int weight;
+	private String color;
+	public Brain(int weight, String color) {
+		super();
+		this.weight = weight;
+		this.color = color;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
+
+class Bike{
+	private String brand;
+	private int milage;
+	
+	public Bike(String brand, int milage) {
+		this.brand = brand;
+		this.milage = milage;
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+	
+	public int getMilage() {
+		return milage;
+	}
+	
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	public void setMilage(int milage) {
+		this.milage = milage;
+	}
+}
+
+class Book{
+	private String name;
+	private String author;
+	
+	public Book(String name, String author) {
+		this.author= author;
+		this.name= name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setName(String name) {
+		this.name= name;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+}
+
+class Human{
+	Heart heart = new Heart(250, 82);
+	Brain brain = new Brain(1400, "Grey");
+	
+	void hasABike(Bike bike) {
+		System.out.println(bike.getBrand());
+		System.out.println(bike.getMilage());
+	}
+	
+	void hasABook(Book book) {
+		System.out.println(book.getName());
+		System.out.println(book.getAuthor());
+		
+	}
+}
+
+public class AggregationAndCompositionDemo {
+	public static void main(String[] args) {
+		Human human = new Human();
+		Bike bike = new Bike("honda", 25);
+		Book book = new Book("Java", "James Gosling");
+		
+		System.out.println(human.heart.getBPM());
+		System.out.println(human.heart.getWeight());
+		System.out.println("========");
+		
+		System.out.println(human.brain.getColor());
+		System.out.println(human.brain.getWeight());
+		System.out.println("========");
+		
+		human.hasABike(bike);
+		System.out.println("========");
+		human.hasABook(book);
+		System.out.println("========");
+		
+		//HUMAN DIED, NO ACCESS TO BRAIN AND HEART
+		human = null;
+//		System.out.println(human.heart.getBPM());
+//		System.out.println(human.heart.getWeight());
+//		System.out.println(human.brain.getColor());
+//		System.out.println(human.brain.getWeight());
+		
+		
+		
+		//HUMAN DIED BUT STILL HAVE ACCESS TO BIKE AND BOOK
+		System.out.println(bike.getBrand());
+		System.out.println(bike.getMilage());
+		System.out.println("========");
+		
+		System.out.println(book.getAuthor());
+		System.out.println(book.getName());
+	}
+	
+	
+	
+}
+
